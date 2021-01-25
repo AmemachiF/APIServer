38253ef03fc8ddab47c3e2a032ad66869b223bf7
Chang Feng <chang_196700@hotmail.com>
2021-01-25 20:58:26 +0800

    feat: add getBilibiliRoomPlayUrl
    Signed-off-by: Chang Feng <chang_196700@hotmail.com>

--- src/cloud.ts
+++ src/cloud.ts
@@ -2,9 +2,13 @@ import AV from 'leanengine'
 import https from 'https'
 
 AV.Cloud.define('getBilibiliLiveInfo', (request) => {
-    const uid = (request.params as any).uid
+    const uid = (request.params as any).id
     return new Promise((resolve, reject) => {
-        https.get(`https://api.live.bilibili.com/live_user/v1/Master/info?uid=${uid}`, (res) => {
+        https.get(`https://api.live.bilibili.com/live_user/v1/Master/info?uid=${uid}`, {
+            headers: {
+                Referer: 'https://www.bilibili.com'
+            }
+        }, (res) => {
             res.on('data', (data) => {
                 resolve(JSON.parse(data))
             })
@@ -19,7 +23,11 @@ AV.Cloud.define('getBilibiliLiveInfo', (request) => {
 AV.Cloud.define('getBilibiliRoomInfo', (request) => {
     const id = (request.params as any).id
     return new Promise((resolve, reject) => {
-        https.get(`https://api.live.bilibili.com/room/v1/Room/room_init?id=${id}`, (res) => {
+        https.get(`https://api.live.bilibili.com/room/v1/Room/room_init?id=${id}`, {
+            headers: {
+                Referer: 'https://www.bilibili.com'
+            }
+        }, (res) => {
             res.on('data', (data) => {
                 resolve(JSON.parse(data))
             })
@@ -32,9 +40,29 @@ AV.Cloud.define('getBilibiliRoomInfo', (request) => {
 })
 
 AV.Cloud.define('getBilibiliRoomInfoOld', (request) => {
-    const uid = (request.params as any).uid
+    const uid = (request.params as any).id
     return new Promise((resolve, reject) => {
         https.get(`https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld?mid=${uid}`, {
+            headers: {
+                Referer: 'https://www.bilibili.com'
+            }
+        }, (res) => {
+            res.on('data', (data) => {
+                resolve(JSON.parse(data))
+            })
+
+            res.on('error', (err) => {
+                reject(err)
+            })
+        })
+    })
+})
+
+AV.Cloud.define('getBilibiliRoomPlayUrl', (request) => {
+    const cid = (request.params as any).id
+    const quality = (request.params as any).quality ?? 2
+    return new Promise((resolve, reject) => {
+        https.get(`https://api.live.bilibili.com/room/v1/Room/playUrl?cid=${cid}&quality=${quality}`, {
             headers: {
                 Referer: 'https://www.bilibili.com'
             }
