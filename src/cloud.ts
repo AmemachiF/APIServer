021d705cf779a709abe89ced4b1e44ae239c2d31
Chang Feng <chang_196700@hotmail.com>
2021-01-25 11:22:32 +0800

    feat: get room info
    Signed-off-by: Chang Feng <chang_196700@hotmail.com>

--- src/cloud.ts
+++ src/cloud.ts
@@ -3,9 +3,23 @@ import https from 'https'
 
 AV.Cloud.define('getBilibiliLiveInfo', (request) => {
     const uid = (request.params as any).uid
-    
     return new Promise((resolve, reject) => {
         https.get(`https://api.live.bilibili.com/live_user/v1/Master/info?uid=${uid}`, (res) => {
+            res.on('data', (data) => {
+                resolve(JSON.parse(data))
+            })
+
+            res.on('error', (err) => {
+                reject(err)
+            })
+        })
+    })
+})
+
+AV.Cloud.define('getBilibiliRoomInfo', (request) => {
+    const id = (request.params as any).id
+    return new Promise((resolve, reject) => {
+        https.get(`https://api.live.bilibili.com/room/v1/Room/room_init?id=${id}`, (res) => {
             res.on('data', (data) => {
                 resolve(JSON.parse(data))
             })
